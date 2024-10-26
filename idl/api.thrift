b52af1299fb73dcc304a21e073ccd5945bdaa9d4
wushiling50 <2531010934@qq.com>
2024-10-26 18:09:24 +0800

    feat: api init
    Signed-off-by: wushiling50 <2531010934@qq.com>

---
+++ idl/api.thrift
@@ -0,0 +1,47 @@
+namespace go api
+
+struct User {
+    1: required string name,
+    2: required double score,
+}
+
+struct TalentRankRequest {
+    1: required string username,
+}
+
+struct TalentRankResponse {
+    1: required i64 status_code = 0,
+    2: optional string status_msg,
+    3: required User user_info,
+}
+
+struct NationGuessRequest {
+    1: required string username,
+}
+
+struct NationGuessResponse {
+    1: required i64 status_code = 0,
+    2: optional string status_msg,
+    3: required string nation,
+    4: required string confidence, // confidence level
+}
+
+struct SearchRequest {
+    1: required string keyword,
+    2: optional list<string> domain_list, // Differentiate using programming languages
+    3: optional list<string> nation_list,
+    4: optional i64 page_size,
+    5: optional i64 page_num,
+}
+
+struct SearchResponse {
+    1: required i64 status_code = 0,
+    2: optional string status_msg,
+    3: required list<User> user_list,
+}
+
+service BasicService {
+    TalentRankResponse TalentRank(1: TalentRankRequest req) (api.get="/aster/talent/rank/")
+    NationGuessResponse NationGuess(1: NationGuessRequest req) (api.get="/aster/nation/guess")
+    SearchResponse Search(1: SearchRequest req) (api.get="/aster/search")
+}
